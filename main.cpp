3fd8326c4bb891364ee77432dec22fe778f36e18
Семенченко ТКИ-142 <neprome3@gmail.com>
2026-04-14 15:53:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,29 +2,40 @@
 #include "Rectangle.h"
 
 using namespace std;
+// Класс Точка
+class Point {
+public:
+    double x, y;
+};
+
+// Функция ввода точки
+Point InputPoint(const char* text) {
+    Point p;
+    cout << text;
+    cin >> p.x >> p.y;
+    return p;
+}
 
 /*
 * @brief Точка входа в программу
 * @return 0, если программа выполнена верно
 */
-int main()
-{
-    double x1, y1, x2, y2, x3, y3, x4, y4;
+int main() {
+    Point p1 = InputPoint("Введите x1 y1: ");
+    Point p2 = InputPoint("Введите x2 y2: ");
+    Point p3 = InputPoint("Введите x3 y3: ");
+    Point p4 = InputPoint("Введите x4 y4: ");
+    if (cin.fail()) {
+        cout << "Ошибка ввода" << endl;
+        return 1;
+    }
 
-    cout << "Input x1 y1: ";
-    cin >> x1 >> y1;
-    cout << "Input x2 y2: ";
-    cin >> x2 >> y2;
-    cout << "Input x3 y3: ";
-    cin >> x3 >> y3;
-    cout << "Input x4 y4: ";
-    cin >> x4 >> y4;
-
-    Rectangle r(x1, y1, x2, y2, x3, y3, x4, y4);
+    Rectangle r(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y, p4.x, p4.y);
 
     r.Show();
     cout << "P = " << r.Perimeter() << endl;
     cout << "S = " << r.Area() << endl;
 
     return 0;
+}
 }
