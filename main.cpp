8925cd1ff10e86d7dd3cf64fede59c9343c67778
Семенченко ТКИ-142 <neprome3@gmail.com>
2026-05-05 02:19:43 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,52 @@
 #include <iostream>
+#include <stdexcept>
+
+#include "Point.h"
 #include "Rectangle.h"
 
-using namespace std;
-// Класс Точка
-class Point {
-public:
-    double x, y;
-};
+bool InputPoint(Point& point)
+{
+    double x;
+    double y;
 
-// Функция ввода точки
-Point InputPoint(const char* text) {
-    Point p;
-    cout << text;
-    cin >> p.x >> p.y;
-    return p;
+    if (!(std::cin >> x >> y))
+    {
+        return false;
+    }
+
+    point = Point(x, y);
+    return true;
 }
 
-/*
-* @brief Точка входа в программу
-* @return 0, если программа выполнена верно
-*/
-int main() {
-    Point p1 = InputPoint("Введите x1 y1: ");
-    Point p2 = InputPoint("Введите x2 y2: ");
-    Point p3 = InputPoint("Введите x3 y3: ");
-    Point p4 = InputPoint("Введите x4 y4: ");
-    if (cin.fail()) {
-        cout << "Ошибка ввода" << endl;
+int main()
+{
+    Point p1;
+    Point p2;
+    Point p3;
+    Point p4;
+
+    std::cout << "Введите координаты 4 точек прямоугольника:" << std::endl;
+
+    if (!InputPoint(p1) || !InputPoint(p2) || !InputPoint(p3) || !InputPoint(p4))
+    {
+        std::cout << "Ошибка ввода данных" << std::endl;
         return 1;
     }
 
-    Rectangle r(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y, p4.x, p4.y);
+    try
+    {
+        Rectangle rectangle(p1, p2, p3, p4);
 
-    r.Show();
-    cout << "P = " << r.Perimeter() << endl;
-    cout << "S = " << r.Area() << endl;
+        rectangle.Show();
+
+        std::cout << "Периметр: " << rectangle.Perimeter() << std::endl;
+        std::cout << "Площадь: " << rectangle.Area() << std::endl;
+    }
+    catch (const std::invalid_argument& error)
+    {
+        std::cout << "Ошибка: " << error.what() << std::endl;
+        return 1;
+    }
 
     return 0;
-}
 }
